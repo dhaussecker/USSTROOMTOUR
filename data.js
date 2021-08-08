e36de6db0267539ee304c2953d77ad2f16e5ae08
djh293 <48139207+djh293@users.noreply.github.com>
2021-08-07 23:22:46 -0600

    first commit

---
+++ data.js
@@ -0,0 +1,409 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrance",
+      "name": "Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.9540446014416784,
+        "pitch": 0.44685449810157607,
+        "fov": 1.3378744097411812
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9540446014416784,
+          "pitch": 0.44685449810157607,
+          "rotation": 0,
+          "target": "1-beside-the-clean-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-beside-the-clean-room",
+      "name": "Beside The Clean Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.368887793843216,
+          "pitch": 0.22768653987535536,
+          "rotation": 0,
+          "target": "2-table"
+        },
+        {
+          "yaw": -0.13027705705714965,
+          "pitch": 0.3815436744250267,
+          "rotation": 0,
+          "target": "5-computers"
+        },
+        {
+          "yaw": -3.128576292862416,
+          "pitch": 0.15770913401372866,
+          "rotation": 0,
+          "target": "3-work-stations"
+        },
+        {
+          "yaw": -1.6001274558171712,
+          "pitch": 0.2712646275113677,
+          "rotation": 0,
+          "target": "4-the-couch"
+        },
+        {
+          "yaw": 1.2664073132937865,
+          "pitch": 0.3116299539168548,
+          "rotation": 0,
+          "target": "0-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.3586789712267002,
+          "pitch": -0.12275325537086879,
+          "title": "The Clean Room",
+          "text": "Our clean room stores the satellite components safely for our cubesat"
+        }
+      ]
+    },
+    {
+      "id": "2-table",
+      "name": "Table",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0550024316259456,
+          "pitch": 0.5782669337352644,
+          "rotation": 0,
+          "target": "4-the-couch"
+        },
+        {
+          "yaw": -1.0576715677482689,
+          "pitch": 0.5490464027792505,
+          "rotation": 0,
+          "target": "1-beside-the-clean-room"
+        },
+        {
+          "yaw": -0.8777243127733456,
+          "pitch": 0.372600375705904,
+          "rotation": 0,
+          "target": "1-beside-the-clean-room"
+        },
+        {
+          "yaw": 0.9204853914040179,
+          "pitch": 0.3131155178642899,
+          "rotation": 0,
+          "target": "3-work-stations"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-work-stations",
+      "name": "Work Stations",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19092437093426895,
+          "pitch": 0.20806483052609792,
+          "rotation": 0,
+          "target": "2-table"
+        },
+        {
+          "yaw": 0.22530884131863083,
+          "pitch": 0.20190110565457253,
+          "rotation": 0,
+          "target": "1-beside-the-clean-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9120513320833599,
+          "pitch": 0.11816397316353466,
+          "title": "USST Mars Rover",
+          "text": "This is our rover which has won 1st in a variety of competitions across the world"
+        }
+      ]
+    },
+    {
+      "id": "4-the-couch",
+      "name": "The Couch",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0428986332417782,
+          "pitch": 0.2056827123847782,
+          "rotation": 0,
+          "target": "2-table"
+        },
+        {
+          "yaw": 1.9990620401794992,
+          "pitch": 0.29132992429369864,
+          "rotation": 0,
+          "target": "1-beside-the-clean-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5838428816079109,
+          "pitch": -0.32514873388192456,
+          "title": "Whiteboard",
+          "text": "This is where the planning begins!"
+        }
+      ]
+    },
+    {
+      "id": "5-computers",
+      "name": "Computers",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5976189235676976,
+          "pitch": 0.4133881603132874,
+          "rotation": 0,
+          "target": "7-the-fridge"
+        },
+        {
+          "yaw": -2.639108700410725,
+          "pitch": 0.2498203988110479,
+          "rotation": 0,
+          "target": "6-the-shelves"
+        },
+        {
+          "yaw": 2.1730533799943927,
+          "pitch": 0.30385135098962124,
+          "rotation": 0,
+          "target": "1-beside-the-clean-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-the-shelves",
+      "name": "The Shelves",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1002045852333087,
+          "pitch": 0.40993269064619753,
+          "rotation": 0,
+          "target": "5-computers"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.268314542043914,
+          "pitch": 0.05517236697018291,
+          "title": "Project STARFOX",
+          "text": "Created to test filtering methods in microgravity"
+        }
+      ]
+    },
+    {
+      "id": "7-the-fridge",
+      "name": "The Fridge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5399404648599937,
+          "pitch": 0.5576457064974925,
+          "rotation": 0,
+          "target": "5-computers"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "USST Room",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
